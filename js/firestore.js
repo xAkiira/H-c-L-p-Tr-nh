74bb2e11aa8cefb710670c4c081992ebbe587dd8
xAkiira <127678035+xAkiira@users.noreply.github.com>
2023-06-15 22:22:56 +0700

    update

--- js/firestore.js
+++ js/firestore.js
@@ -53,21 +53,98 @@ import { getFirestore,  collection, getDocs , addDoc, deleteDoc , doc} from "htt
    const createData = async (db, object) => {
 
   try {
-  const docRef = await addDoc(collection(db, "cart"), {
-    product:"product/O2NpHOLkEYItbB7oscNn",
-    quantity: 2
+  const docRef = await addDoc(collection(db, "products"), {
+        name: object.name,
+        price: object.price,
+        rate: object.rate,
+        image:object.image,
   });
   console.log("Document written with ID: ", docRef.id);
 } catch (e) {
   console.error("Error adding document: ", e);
 }
    }
+  // thêm dữ liệu vào cart
+  const createitem = async (db, object) => {
+
+    try {
+    const docRef = await addDoc(collection(db, "cart"), {
+      product:object.product,
+      quantity:object.quantity,
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+     }
+     const obj = {quantity: 2,product: "a49Mvce7NkI9sOVd6REs"}
+     document.getElementById("add").addEventListener("click",await createitem(db, obj))
   // xóa dữ liệu
    const deleteDataById = async (db, id) => {
     deleteDoc(doc(db, "cart", id));
    }
   //test
-//   await createData(db, "s").then(rs => rs).catch(e => console.log(e));
-  document.getElementById('app').innerHTML += await getData(db).then(rs => rs);
+  const productslist = [
+    {
+        name: "Armchair Bridge Gỗ Tự nhiên Da đen",
+        price: "66,200,000đ",
+        rate: 4.5,
+        image:"https://nhaxinh.com/wp-content/uploads/2021/10/armchair_bridge_black_1.jpg"
+    },
+    {
+        name: "Tủ tivi Bridge Gỗ 1m8 Màu nâu",
+        price: "56,000,000đ",
+        rate: 4.5,
+        image:"https://nhaxinh.com/wp-content/uploads/2021/10/1000-san-pham-nha-xinh48-600x400.jpg"
+    },
+    {
+        name: "Ghế Bar Bridge màu nâu Da Cognac",
+        price: "30,500,000đ",
+        rate: 4.5,
+        image:"https://nhaxinh.com/wp-content/uploads/2022/05/ghe-bar-bridge-mau-nau-cognac-go-soi.jpg"
+    },
+    {
+        name: "Tủ ly Bridge gỗ màu Tự nhiên",
+        price: "57,700,000đ",
+        rate: 4.5,
+        image:"https://nhaxinh.com/wp-content/uploads/2021/10/1000-tu-tivi-bridge-600x400.jpg"
+    },
+    {
+        name: "Bàn nước Bridge mặt Marble đen 120cm",
+        price: "37,506,000đ",
+        rate: 4.5,
+        image:"https://nhaxinh.com/wp-content/uploads/2021/10/84909-1000x666-1-600x400.jpg"
+    },
+    {
+        name: "Console Bridge P4C Onyx",
+        price: "39,990,000đ",
+        rate: 4.5,
+        image:"https://nhaxinh.com/wp-content/uploads/2021/10/82528-1000x666-1-600x400.jpg"
+    },
+    {
+        name: "Sofa Bridge 3 chỗ hiện đại da đen",
+        price: "112,000,000đ",
+        rate: 4.5,
+        image:"https://nhaxinh.com/wp-content/uploads/2021/10/sofa-bride-go-goi-da-bo-that-cao-cap-hien-dai-dang-cap-sang-trong-600x400.jpg"
+    },
+    {
+        name: "Giường Hộc Kéo Iris 1M8 Vải Belfast 41",
+        price: "15,900,000đ",
+        rate: 4.5,
+        image:"https://nhaxinh.com/wp-content/uploads/2021/10/giuong_iris_1m6_stone3.jpg"
+    },
+    {
+        name: "Kệ sách Glamour màu trắng",
+        price: "25,200,000đ",
+        rate: 4.5,
+        image:"https://nhaxinh.com/wp-content/uploads/2021/11/61148_ke_sach_glamour_mau_trang_l2.jpg"
+    },
+    ]
+    // for (let index = 0; index < productslist.length; index++) {
+    //   const element = array[index];
+      
+    // }
+  await createData(db, "s").then(rs => rs).catch(e => console.log(e));
+  // document.getElementById('app').innerHTML += await getData(db).then(rs => rs);
 //   console.log(await getDataById(db , "O2NpHOLkEYItbB7oscNn").then(rs => rs));
-  await deleteDataById(db, "Gv0COHiYuxfOAUFXivaZ");
+  // await deleteDataById(db, "Gv0COHiYuxfOAUFXivaZ");
